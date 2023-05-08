47fc559b8838e94f4889822d73ed1b98f56b8473
khagiya1 <k.hagiya@nttcoms.com>
2023-05-08 17:05:46 +0900

    デフォルトのRequestOptionsの構築対象にapi_versionも含める

--- src/test/java/jp/karaden/TestHelper.java
+++ src/test/java/jp/karaden/TestHelper.java
@@ -4,11 +4,13 @@ public class TestHelper {
     public static final String API_BASE = "http://localhost:4010";
     public static final String API_KEY = "123";
     public static final String TENANT_ID = "159bfd33-b9b7-f424-4755-c119b324591d";
+    public static final String API_VERSION = "2023-01-01";
 
     public static RequestOptions.Builder getDefaultRequestOptionsBuilder() {
         return RequestOptions.newBuilder()
             .withApiBase(TestHelper.API_BASE)
             .withApiKey(TestHelper.API_KEY)
-            .withTenantId(TestHelper.TENANT_ID);
+            .withTenantId(TestHelper.TENANT_ID)
+            .withApiVersion(TestHelper.API_VERSION);
     }
 }
